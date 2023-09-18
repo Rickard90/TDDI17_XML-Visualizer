61cac9e2f1ce2ee536600b5676bc03eb366e0f43
Oskar Sjöberg <osksj747@student.liu.se>
2023-09-18 16:04:14 +0200

    Skapat en grundläggande definition för component-klassen

---
+++ Component.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Component
+{
+	public Component()
+	{
+	}
+
+	public virtual void Draw(position in pos);
+
+ private:
+	string			component_name	= new();
+    float			graphicsize		= 0;
+    component		parent			= new();
+	list{component} children		= new();
+	list{component}	myConnections	= new();
+		
+}
