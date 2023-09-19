5454fb87bb87ef272326f4b98065395813438f44
Oskar Sjöberg <osksj747@student.liu.se>
2023-09-19 16:00:52 +0200

    Uppdaterad component.cs, gjort component abstract och skapat en väldigt enkel konstruktor. Också lagt in en ny klass 'top' i samma fil. Ska flytta den till egen fil senare

--- src/Component.cs
+++ src/Component.cs
@@ -1,20 +1,49 @@
 ﻿using System;
+using System.Linq.Expressions;
 using Microsoft.Xna.Framework;
 
-public class Component
+
+//All types of components inherit constructor and fields from the component-type
+public abstract class Component
 {
 	public Component()
 	{
 	}
+	public Component(string name,
+					 List<Component> children)
+	{
+		this.componentName	= name;
+		this.children		= children;
+	}
 
-	public virtual void Draw(Point pos) {}
 
-/*
- private:
-	string			component_name	= new();
-    float			graphicsize		= 0;
-    component		parent			= new();
-	list{component} children		= new();
-	list{component}	myConnections	= new();
-*/		
+	public abstract void Draw(Point pos);
+
+ 
+	private	readonly string				componentName	= "";
+	private 		 float				graphicSize		= 0;
+	private  		 Component			parent			= new();
+	private	readonly List<Component> 	children		= new();
+	private	readonly List<Component>	myConnections	= new();
+		
+}
+
+//Top is an object that keeps track of a full loaded topography
+//A topography is stored as a tree with a top component
+public class Top
+{
+
+	public Top(Component head)
+	{
+		this.currentComponent 	= head;
+		this.head 				= head;
+	}
+
+	public void Goto(Component newComponent)
+	{
+		currentComponent = newComponent;
+	}
+
+	private 		 Component currentComponent	= new();
+	private readonly Component head 			= new(); 
 }
